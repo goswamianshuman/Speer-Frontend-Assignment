2bf6afcf3500b52c12ad1f6fadcc1a5362ee4559
Anshuman Goswami <anshumangoswami07@gmail.com>
2024-01-05 00:45:13 +0530

    fix: page switch error

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./styles/index.css";
 import { ThemeProvider } from "./lib/context/theme-provider.jsx";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./lib/components/elements/navbar.jsx";
 import FeedPage from "./routes/home.jsx";
 import ArchivePage from "./routes/archive.jsx";
@@ -10,15 +10,15 @@ import { Toaster } from "./lib/components/ui/sonner";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-    <BrowserRouter>
+    <Router>
       <div className="max-w-6xl w-full mx-auto relative">
         <Navbar />
         <Routes>
-          <Route path="/" element={<FeedPage />} />
-          <Route path="/archives" element={<ArchivePage />} />
+          <Route exact path="/" element={<FeedPage />} />
+          <Route exact path="/archives" element={<ArchivePage />} />
         </Routes>
       </div>
       <Toaster />
-    </BrowserRouter>
+    </Router>
   </ThemeProvider>
 );
